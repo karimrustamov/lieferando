1ad1571891d758c318f45ef2ebe755290eac3c2a
Karim <karimrustamov@homail.de>
2023-09-15 16:08:54 +0200

    first

---
+++ lieferandoScript.js
@@ -0,0 +1,64 @@
+{
+    "food": [
+        {
+            "name": "Pizza Margherita",
+            "ingredients": "Tomatensauce, Mozzarella, Basilikum",
+            "price": "8,00€"
+        },
+        {
+            "name": "Caesar Salad",
+            "ingredients": "Römersalat, Caesar-Dressing, Croutons, Parmesan",
+            "price": "7,00€"
+        },
+        {
+            "name": "Burger Classic",
+            "ingredients": "Rindfleischpatty, Salat, Tomate, Gurke, Käse, Burger-Sauce",
+            "price": "9,50€"
+        },
+        {
+            "name": "Spaghetti Carbonara",
+            "ingredients": "Spaghetti, Speck, Ei, Parmesan, Pfeffer",
+            "price": "8,00€"
+        },
+        {
+            "name": "Sushi-Set",
+            "ingredients": "Verschiedene Sushi-Rollen, Wasabi, Sojasauce",
+            "price": "14,00€"
+        },
+        {
+            "name": "Griechischer Salat",
+            "ingredients": "Tomaten, Gurken, Oliven, Feta, Zwiebeln",
+            "price": "7,00€"
+        },
+        {
+            "name": "Schokoladenkuchen",
+            "ingredients": "Schokolade, Mehl, Zucker, Eier",
+            "price": "4,50€"
+        },
+        {
+            "name": "Cola",
+            "ingredients": "Getränk: 500ml",
+            "price": "2,50€"
+        },
+        {
+            "name": "Vegane Pasta",
+            "ingredients": "Vollkornpasta, Gemüsemischung, Tomatensauce",
+            "price": "9,00€"
+        },
+        {
+            "name": "Chicken Wings",
+            "ingredients": "Hühnerflügel, Gewürze, Sauce nach Wahl",
+            "price": "7,50€"
+        },
+        {
+            "name": "Fisch-Taco",
+            "ingredients": "Fisch, Tortilla, Salat, Tomate, Zwiebel, Sauce",
+            "price": "8,50€"
+        },
+        {
+            "name": "Fruchtiger Smoothie",
+            "ingredients": "Verschiedene Früchte, Joghurt",
+            "price": "5,00€"
+        }
+    ]
+}
